832e1121f37aee71211a9e3a4b68fa5432a36609
Pedro Augusto Alves Silva <pedro.augusto77@hotmail.com>
2024-12-16 21:50:43 -0300

    more levels

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -3,3 +3,4 @@ extends LeveParent
 func _on_gate_player_etered_gate(_body: Variant) -> void:
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
